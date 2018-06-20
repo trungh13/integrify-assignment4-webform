3737f3639d0707db41c40c69fce4ae0cf8b678e6
trungh13 <newbie.trungh13@gmail.com>
2018-06-20 13:41:25 +0300

    add css-modules, react-router-dom, Header Component

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,10 +1,20 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import styles from "./App.css";
+import {
+  BrowserRouter as Router,
+  Route,
+  NavLink,
+  Redirect,
+  Prompt,
+  Switch
+} from "react-router-dom";
+import Header from "./Header";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className={styles.App}>
+        <Header />
       </div>
     );
   }
---
+++ src/Components/ComponentHeader/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import styles from "./index.css";
+
+const Header = () => {
+  return (
+    <div className={styles.Header}>
+      <h1>Let's talk</h1>
+      <h3>Think you have what it takes? Show us</h3>
+    </div>
+  );
+};
+
+export default Header;
