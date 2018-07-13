7c5e52fe711262a37e1c55df2f023b68fdc19284
Unknown <newbie.trungh13@gmail.com>
2018-07-13 09:32:45 +0300

    Change structure for Redux and delete files

---
+++ src/containers/Header/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import styles from './index.css';
+
+const Header = () => (
+    <div className={styles.Header}>
+      <h1>Let&apos;s talk</h1>
+      <h3>Think you have what it takes? Show us</h3>
+    </div>
+);
+
+export default Header;
